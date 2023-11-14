99b5ec744f679e0495aceb50b363210b7fc8d7bc
pirksa <ato.3.141k@gmail.com>
2023-11-14 22:40:30 +0600

    BB-0004. Updated bot.py under aiogram 3.1.1.

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
+import asyncio
+import logging
+import sys
+
 from aiogram import Bot, types, Dispatcher
-from aiogram.utils import executor
+from aiogram.filters import CommandStart
 
-bot = Bot(token='6616305229:AAHj3KN7esge2F2PIDStruPUqXTO1mTQ5Jg')
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=['start'])
+@dp.message(CommandStart)
 async def start_command(message: types.Message):
     await message.answer('Greetings!')
 
 
+async def main():
+    bot = Bot(token='6616305229:AAHj3KN7esge2F2PIDStruPUqXTO1mTQ5Jg')
+    await dp.start_polling(bot)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
